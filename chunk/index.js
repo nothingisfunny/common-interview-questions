c5dc324f9bcce4643f5619b855036649b3265863
nothingisfunny <if.the.kids.r.united@gmail.com>
2018-03-05 19:50:51 -0800

    add second solution with slice to array chunk

--- chunk/index.js
+++ chunk/index.js
@@ -1,7 +1,20 @@
+//solution 1 (splice)
 function chunk(array, size) {
   let newArr = []
   while(array.length > 0){
     newArr.push(array.splice(0, size));
+  }
+  return newArr;
+}
+
+
+//solution 2 (slice)
+function chunk(array, size) {
+  let newArr = []
+  let index = 0;
+  while(index < array.length){
+    newArr.push(array.slice(index, index+size));
+    index += size;
   }
   return newArr;
 }
