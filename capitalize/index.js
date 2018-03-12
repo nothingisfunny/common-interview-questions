819a0b00b5fad354ea0ab6c84a2002d4a1ccbd46
nothingisfunny <if.the.kids.r.united@gmail.com>
2018-03-12 14:28:42 -0700

    add second capitalize solution

--- capitalize/index.js
+++ capitalize/index.js
@@ -5,3 +5,16 @@
 function capitalize(str) {
   return str.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ")
 }
+
+
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++){
+    if (str[i - 1] === ' '){
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+}
