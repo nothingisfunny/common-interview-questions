6e4282ff3dbeac6116c5cd10f651df3848c84a47
nothingisfunny <if.the.kids.r.united@gmail.com>
2018-03-12 21:56:04 -0700

    add better solution to steps using rows and columns concept

--- steps/steps.js
+++ steps/steps.js
@@ -1,14 +1,30 @@
-function steps(n) {
+// function steps(n) {
   
-  for (let i = 0; i < n; i++){
-    var string = ''
-    for (let j = i+1; j>0; j--){
-      string += '#'
-    }
-    for (let j = n-i; j>1; j--){
-      string += ' '
-    }
+//   for (let i = 0; i < n; i++){
+//     var string = ''
+//     for (let j = i+1; j>0; j--){
+//       string += '#'
+//     }
+//     for (let j = n-i; j>1; j--){
+//       string += ' '
+//     }
 
+//     console.log(string)
+//   }
+// }
+
+//better solution 
+
+function steps(n){
+  for (let row = 1; row < n + 1; row++){
+    let string = ''
+    for (let col = 1; col < n + 1; col++){
+      if(col <= row){
+        string += '#'
+      }else{
+        string+= ' '
+      }
+    }
     console.log(string)
   }
 }
