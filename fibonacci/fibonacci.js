4f25c7bb94f3bbd8707a12f3e81a2a9753974bdf
nothingisfunny <if.the.kids.r.united@gmail.com>
2018-03-25 21:23:51 -0700

    add fibonacci problem with interative solution

---
+++ fibonacci/fibonacci.js
@@ -0,0 +1,15 @@
+// Return an n element in fibonacci sequence.
+
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+function fib(n) {
+  let arr = [0, 1]
+    for(let i = 2; i<n+1; i++){
+      arr.push(arr[i - 2] + arr[i - 1])
+    }
+  return arr[n]
+}
