451ff5f4dada9f95813cba333d8b7235bd803616
nothingisfunny <if.the.kids.r.united@gmail.com>
2018-04-02 10:58:08 -0700

    add selection sort solution

--- vowels/vowels.js
+++ vowels/vowels.js
@@ -15,23 +15,3 @@ function vowels(str){
   let matches = str.match(/[aeiou]/gi)
   return matches ? matches.length : 0
 }
-
-def rot13(secret_messages)
-  alph = Array('a'..'z')
-  return secret_messages.collect do |message|
-      mes = message.downcase.split("").collect do |let|
-          if let != " " || let.scan(/\w/) != []
-              index = alph.find_index(let)
-              if index < alph.length
-                  alph[13 - (alph.length - index)]
-              else
-                  alph[index + 13]
-              end
-          else
-              let
-          end
-      end
-       binding.pry
-      mes.join("").capitalize
-  end
-end
